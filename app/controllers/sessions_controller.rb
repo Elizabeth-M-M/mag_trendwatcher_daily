9c32eeabf7e4038e97c40a309470e0d41211976e
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-27 13:44:55 +0300

    Added the controller methods for the defined sessions routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+   #POST /login
+  def create
+    user= User.find_by(username:params[:username])
+    if user&.authenticate(params[:password])
+      session[:user_id]=user.id
+      render json: user
+    else
+      render json: {errors:["Wrong username or password"]}, status: :unprocessable_entity #422 
+    end
+  end
+  #DELETE /logout
+  def destroy
+    session.delete :user_id
+    head :no_content
+  end
 end
