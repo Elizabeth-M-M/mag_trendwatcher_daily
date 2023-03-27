b925d778520a7448b03764d581c8ab0e45303743
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-27 15:58:39 +0300

    Defined methods required in the Article routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,19 @@
 class ArticlesController < ApplicationController
+  rescue_from ActiveRecord::RecordNotFound, with: :article_not_found
+
+  def index 
+    render json: Article.all
+  end
+  def show
+    article= find_article
+    render json: article
+  end
+  private
+  def find_article
+    Article.find(params[:id])
+  end
+  #Method that renders error message from a non existent article in database
+    def article_not_found
+      render json: {errors:["Article not found"]}, status: :not_found  #404
+    end
 end
