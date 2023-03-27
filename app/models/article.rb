45ecf93f106d342ceeebc5d4178faa8c01c96fb9
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-27 15:37:56 +0300

    Added Article validations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+  validates :title, :image, presence: true
+  validates :content, length: {minimum:500}
+  validates :category, inclusion: {in: %(Lifestyle Food Technology Sport Business Gaming Travel)}
 end
