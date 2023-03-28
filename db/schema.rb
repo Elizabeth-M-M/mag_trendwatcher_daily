fdefb101cae54b1df8a9816bb2d5b69bd98efb65
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-28 19:58:50 +0300

    Added Editor seed data to the seeds.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_28_115843) do
+ActiveRecord::Schema.define(version: 2023_03_28_165153) do
 
   create_table "articles", force: :cascade do |t|
     t.string "image"
     t.text "content"
     t.string "title"
     t.string "category"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "editors", force: :cascade do |t|
+    t.string "username"
+    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
