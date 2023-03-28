e93f4f51071ad4fa89c1ef1e41da5fbf6dfb1380
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-28 20:01:07 +0300

    Redefined users login and editor login route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   
-  resources :editors
+  
   resources :reviews, only: :create
   resources :articles, only: [:show, :index]
   post "/signup", to: "users#create"
   get "/me", to: "users#show"
-  post "/login", to: "sessions#create"
+  post "/login/user", to: "sessions#create"
+  post "/login/editor", to: "editors#create"
   delete "/logout", to: "sessions#destroy"
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
