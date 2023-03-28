953307636e6ca0bfd8e40e9d5f557c67f8465075
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-28 15:10:57 +0300

    Defined the three model relationship

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,7 @@
 class Article < ApplicationRecord
+  has_many :reviews
+  has_many :users, through: :reviews
+
   validates :title, :image, presence: true
   validates :content, length: {minimum:500}
   validates :category, inclusion: { in: %w(Lifestyle Food Technology Sport Business Gaming Travel Science) }
