71e29e42650d792369473b81e5022a2e18502f99
Elizabeth <mwendemuthusi6@gmail.com>
2023-03-29 07:56:45 +0300

    Defined the added method routes for the Article model

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :article_not_found
+  rescue_from ActiveRecord::RecordInvalid, with: :invalid_article_credentials
 
   def index 
     render json: Article.all, each_serializer:ArticlePlusContentSerializer
@@ -8,12 +9,35 @@ class ArticlesController < ApplicationController
     article= find_article
     render json: article, include: ['reviews','reviews.user'] 
   end
+  def create
+    article= Article.create!(article_params)
+    render json: article, serializer:ArticlePlusContentSerializer
+  end
+  def destroy
+    article= find_article
+    article.destroy
+    head :no_content
+  end
+  def update
+    article= find_article
+    article.update!(article_params)
+    render json: article, serializer:ArticlePlusContentSerializer
+  end
   private
+  #find article using id
   def find_article
     Article.find(params[:id])
   end
+   #article parameters required
+    def article_params
+      params.permit(:title, :image, :content, :category)
+    end
   #Method that renders error message from a non existent article in database
     def article_not_found
       render json: {errors:["Article not found"]}, status: :not_found  #404
+    end
+     #Method that renders error message from invalid article details
+    def invalid_article_credentials(invalid)
+      render json: {errors:invalid.record.errors.full_messages}, status: :unprocessable_entity #422
     end
 end
